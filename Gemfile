cd074a459b189e615d1068ac6c77a04414245ba1
Franz Torghele <f.torghele@gmail.com>
2011-04-07 17:23:15 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'mysql2'
+gem 'jquery-rails'
+
+group :development, :test do
+
+end
+
+group :production do
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodCorp::Application.config.secret_token = 'aded04164dfb907772c833a2bb409abc7bcfd5c01e291b23cc57cf72283ba8fee63ef9e86bc83619bcc2e14459e9bf9e07636f7722b001291991cea18ad2ce7f'

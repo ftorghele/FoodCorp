fc69ee79b3a6c6c7102a5854dd3f0dac41c9f5bf
Franz Torghele <f.torghele@gmail.com>
2011-04-10 18:11:25 +0200

    grap some unserdata from fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :sex, :city, :state, :country
 
 
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
@@ -13,15 +14,16 @@ class User < ActiveRecord::Base
     if user = User.find_by_email(data["email"])
       user
     else # Create a user with a stub password.
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
-    end
-  end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["user_hash"]
-        user.email = data["email"]
-      end
+      address = data["hometown"]["name"].split(", ")
+      User.create!( :email => data["email"],
+                    :password => Devise.friendly_token[0,20],
+                    :first_name => data["first_name"],
+                    :last_name => data["last_name"],
+                    :sex => data["gender"],
+                    :city => address[0],
+                    :state => address[1],
+                    :country => address[2]
+                  )
     end
   end
 end

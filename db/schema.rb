d02d354b7851e681736e51bcc338618145ef9c1b
Manuel Gottstein <gotschi@gmail.com>
2011-04-20 12:30:27 +0200

    Cartographer initialized in Taj Mahal

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20110419183114) do
-
 
   create_table "fellowships", :force => true do |t|
     t.integer  "user_id"

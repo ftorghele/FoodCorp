1ae375af420720271b1422305fc6040354da9a55
Wolfgang Vogl <wuulf01@yahoo.de>
2011-06-07 11:44:28 +0200

    updated home

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -4,6 +4,15 @@ class AjaxController < ApplicationController
     :html
   end
 
+  def beginning_to_end_of_day(date_int)
+    date = Time.at(date_int.to_time.to_i)
+    from = date
+    to = date + 1.day
+    puts from
+    puts to
+    from..to
+end
+
   def update_map
     @searchLocation = params[:searchLocation]
     @radius = params[:searchRadius].to_i
@@ -24,8 +33,19 @@ class AjaxController < ApplicationController
     @lat = params[:searchLat].to_f
     @lon = params[:searchLon].to_f
     
-    @meals = Meal.near([@lat, @lon], @radius).find(:all, :limit => 10, :conditions => ["deadline > ?", Time.now.to_datetime.to_i])
+    @date_time = params[:time]
+    puts params[:time] 
 
+    if params[:time] == ""  #TODO
+      @meals = Meal.near([@lat, @lon], @radius).find(:all, :limit => 10, :conditions => ["deadline > ? AND time = ?..?", Time.now.to_datetime.to_i ,  Time.at(params[:time])-1.day.to_datetime.to_i, Time.at(params[:time])+1.day.to_datetime.to_i ])
+    else
+      @meals = Meal.near([@lat, @lon], @radius).find(:all, :limit => 10, :conditions => ["deadline > ?", Time.now.to_datetime.to_i])
+    end
+
+
+    @meals.each do |m|
+      m.distance = Geocoder::Calculations.distance_between([@lat, @lon], [m.lat, m.lon]) * 1.609344
+    end
   end
 
   def calendar
@@ -44,6 +64,5 @@ class AjaxController < ApplicationController
       @user_tasks << ["meal_id"=>meal_arrangement.meal_id, "own"=>false, "time"=>meal_arrangement.meal.time, "title"=>meal_arrangement.meal.title, "cook"=>meal_arrangement.meal.user.first_name ]
     end
     @user_tasks.sort! { |a,b| a[0]["time"] <=> b[0]["time"] }
-
   end
 end

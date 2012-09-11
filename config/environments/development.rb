337d2d1c39dbacf942dc9a0dc3549713203370eb
Barbara Huber <huber.barbara@gmail.com>
2012-09-11 19:54:33 +0200

    show user anpassen 	modified:   app/views/users/_provided_meals.html.erb 	modified:   config/environments/development.rb 	modified:   config/locales/en.yml 	modified:   config/locales/pirate.yml 	modified:   db/schema.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,20 @@ FoodCorp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'dinnercollective.com' }
+  config.action_mailer.smtp_settings = {
+        :enable_starttls_auto => true,
+        :address =>"smtp.gmail.com",
+        :port => 587,
+        :domain => "dinnercollective.com",
+        :authentication => :plain,
+        :user_name => "info@dinnercollective.com",
+        :password => "dinner28"
+} 
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
